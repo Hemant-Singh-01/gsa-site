45aec6e1ab19bc8e9f850362b6a320a9ab7fa78f
Hemant-Singh-01 <hemu3842@gmail.com>
2025-10-18 12:27:40 +0530

    Update prompts.ts

--- data/prompts.ts
+++ data/prompts.ts
@@ -4,9 +4,9 @@ export type Prompt = {
 };
 
 export const prompts: Prompt[] = [
-  { title: "Prompt 1", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=15" },
-  { title: "Prompt 2", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=14" },
-  { title: "Prompt 3", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=13" },
-  { title: "Prompt 4", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=16" },
-  { title: "Prompt 5", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=17" },
+  { title: "Prompt 1", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=18" },
+  { title: "Prompt 2", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=19" },
+  { title: "Prompt 3", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=20" },
+  { title: "Prompt 4", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=21" },
+  { title: "Prompt 5", url: "https://aiskillshouse.com/student/qr-mediator.html?uid=11196&promptId=22" },
 ];
